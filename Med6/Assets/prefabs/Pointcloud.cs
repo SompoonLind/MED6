5ddef3e5e4ba014d6ef7b745d02b90f6db74c8d3
Hatzi2 <72734884+Hatzi2@users.noreply.github.com>
2023-03-13 12:04:55 +0100

    continiued csv

--- Med6/Assets/prefabs/Pointcloud.cs
+++ Med6/Assets/prefabs/Pointcloud.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+using System.IO;
 
 public class Pointcloud : MonoBehaviour
 {
     // Start is called before the first frame update
     public float rayDistance = 5f;
     private Camera cam;
-
+    string filename = "";
+    bool headerLine = true;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
         cam = this.GetComponent<Camera>();
-       
+        var curTime = "";
+        curTime = System.DateTime.Now.ToString("dd-MM-yyyy_hh-mm-ss");
+        
+        //filename = Application.dataPath + "/CSVFiles/test2.csv";
+        filename = Application.dataPath + "/CSV/" + curTime +".csv";
     }
 
     // Update is called once per frame
@@ -21,19 +28,39 @@ public class Pointcloud : MonoBehaviour
         RaycastHit hit;
         Ray ray = new Ray(cam.transform.position, cam.transform.forward);
         Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.red);
+        
         if (Physics.Raycast(ray, out hit)) {
-            Debug.Log(hit.point);
+            WriteCSV();
+            //Debug.Log(hit.point[0]);
             //GameObject primitive = GameObject.CreatePrimitive(PrimitiveType.Sphere);
             //Destroy(primitive.GetComponent<Collider>());
             //primitive.transform.position = hit.point;
             //primitive.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
             //primitive.GetComponent<Renderer>().material = mat;
-            
-
-            
-
-
         }
 
+
+    }
+        public void WriteCSV()
+        {
+        
+        TextWriter tw = new StreamWriter(filename, true);
+
+        if (headerLine == true)
+        {
+            tw.WriteLine("X, Y, Z"); //Add to this list if we want to add more predetermined things
+            tw.Close();
+            tw = new StreamWriter(filename, true);
+            headerLine = false;
+        }
+            
+        for (int i = 0; i < 1; i++)
+        {
+            tw.WriteLine(1 + ";" + 2 + ";" + 3); //Add to this list if we want to add more predetermined things
+        }
+   
+        tw.Close();
+        
     }
 }
+
