9474b32ce9473da5094aa793972165a8c0f71747
kristian1402 <ksaren20@student.aau.dk>
2023-03-15 10:45:06 +0100

    disse

--- Med6/Assets/prefabs/PointVisualizer.cs
+++ Med6/Assets/prefabs/PointVisualizer.cs
@@ -2,30 +2,93 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System.IO;
+using System.Linq;
 
-public class Pointvisualizer : MonoBehaviour
+/*
+How to use: ved kørsel af programmet finder den selv min og maks, sætter dem i inspectoren, og tegner derefter den fulde "visualization". 
+Hvis man kun vil se en given tidsperiode, skriver man først sin nye ønskede min og max værdi, og trykker derefter på "Reset spheres" checkboxen
+*/
+public class PointVisualizer : MonoBehaviour
 {
-    string filePath = Application.dataPath + "/CSV/test.csv";
-    StreamReader reader;
+    public float Min; 
+    public float Max;
+    public bool ResetSpheres = false; //Knap til at resette spheres
+    string filePath; //Fil placering
+    StreamReader reader; //Læs fil
+    string[][] data; //2d Array til at splitte alle linjer i CSV filen til individuelle lister for X, Y, Z og tid
+    List<float> XValues = new List<float>(); //Liste til alle X-værdier
+    List<float> YValues = new List<float>(); //Liste til alle Y-værdier
+    List<float> ZValues = new List<float>(); //Liste til alle Z-værdier
+    List<float> timeValues = new List<float>(); //Liste til alle tid værdier
+    bool spheresDrawn = false; //Bolean så spheres kun tegnes en gang
+    GameObject SphereController; //GameObject der sættes som parent for alle spawnede spheres 
 
     void Start()
     {
-        reader = new StreamReader(filePath);
+        SphereController = new GameObject("Sphere Controller"); //Opretter GameObject til at Parente alle spheres, bare så det ser lidt pænere ud i Hierarchy
+        filePath = Application.dataPath + "/CSV/test.csv"; //Filplacering af CSV fil, skal gøres lidt mere modulært
+        reader = new StreamReader(filePath); //Læs fil på filplacering
+
+        reader.ReadLine(); //Skip header linjen
+
+        string[] lines = reader.ReadToEnd().Split("\n"[0]); //Læs alle linjer til et array og split ved newline
+        data = new string[lines.Length][]; //2d array med antal linjer i CSV filen og værdien på hver af dem
+        for (int i = 0; i < lines.Length; i++) { 
+            data[i] = lines[i].Split(";"[0]); //For loop der splitter værdierne ved semikolon, så vi får de individuelle
+        }
+        
+        for (int i = 0; i < data.Length-1; i++) //For loop der opdeler værdierne i hver ders liste frem for et 2d array
+        {
+            XValues.Add(float.Parse(data[i][0]));
+            YValues.Add(float.Parse(data[i][1]));
+            ZValues.Add(float.Parse(data[i][2]));
+            timeValues.Add(float.Parse(data[i][3]));            
+        }
+        Min = timeValues.First(); //Sætter minimum værdien fra CSV filen 
+        Max = timeValues.Last(); //Sætter maksimum værdien fra CSV filen 
     }
 
-    // Update is called once per frame
     void Update()
     {
-        while(!reader.EndOfStream)
+        //Hvis der ikke er tegnet spheres, tegn dem, hvis der bliver trykket reset, destroy dem og tegn dem igen
+        if (spheresDrawn == false)
         {
-            string line = reader.ReadLine();
-            string[] values = line.Split(";");
-            GameObject primitive = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            Destroy(primitive.GetComponent<Collider>());
-            Debug.Log(values);
-            //primitive.transform.position = values;
-            //primitive.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            drawSpheres();
         }
+        if (ResetSpheres)
+        {
+            Destroy();
+        }
+    }
 
+    void drawSpheres()
+    {
+        for (int i = 0; i < data.Length-1; i++) //For loop på data.length-1 så vi sætter værdien for hver individuel koordinat
+        {
+            if (timeValues[i] > Min && timeValues[i] < Max) //Tegn kun dem hvis tid falder indenfor min og max
+            {
+                //Opretter, tagger, farver og positionerer spheres ud fra CSV data
+                GameObject primitive = GameObject.CreatePrimitive(PrimitiveType.Sphere); 
+                primitive.transform.tag = "visualizer";
+                primitive.transform.parent = SphereController.gameObject.transform;
+                primitive.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.red);
+                Destroy(primitive.GetComponent<Collider>());
+                primitive.transform.position = new Vector3(XValues[i], YValues[i], ZValues[i]);
+                primitive.transform.localScale = new Vector3(0.2f, 0.2f, 0.2f);
+            }
+        }
+        spheresDrawn = true;
+    }
+
+    void Destroy() //Finder alle gameobjects med Tagget "visualizer" og destroyer dem, og tegner derefter nye
+    {
+        GameObject[] destroyTag;
+        destroyTag = GameObject.FindGameObjectsWithTag("visualizer");
+        foreach (GameObject destroy in destroyTag)
+        {
+            Destroy(destroy);
+        }
+        ResetSpheres = false;
+        spheresDrawn = false;
     }
 }
