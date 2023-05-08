f3d6f84e0dfd44c917c9800cc2f47d7ec301131c
kristian1402 <ksaren20@student.aau.dk>
2023-05-08 14:00:18 +0200

    movement test

---
+++ Med6/Assets/Scripts/VisualController.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class VisualController : MonoBehaviour
+{
+    [Header("Only tick one at a time")]
+    public bool writeData = false;
+    public bool particles = false;
+    public bool primitives = false;
+    public bool followAlong = false;
+    Camera mainCamera;
+    GameObject player;
+    GameObject writerController;
+    GameObject particleController;
+    GameObject primitivesController;
+    GameObject followController;
+
+    void Start()
+    {
+        writerController = GameObject.FindGameObjectWithTag("Player");
+        CSVWriter setWriteTrue = writerController.GetComponent<CSVWriter>();
+
+        particleController = GameObject.Find("Particle System");
+        primitivesController = GameObject.Find("point visualizer");
+        followController = GameObject.Find("Camera Movement");
+        
+        player = GameObject.Find("Player");
+        mainCamera =  Camera.main;
+        mainCamera.enabled = false;
+
+        if (writeData == true)
+        {
+            particleController.SetActive(false);
+            primitivesController.SetActive(false);
+            followController.SetActive(false);
+        }
+
+        if (particles == true)
+        {
+            setWriteTrue.enabled = false;
+            primitivesController.SetActive(false);
+            followController.SetActive(false);
+        }
+
+        if (primitives == true)
+        {
+            setWriteTrue.enabled = false;
+            particleController.SetActive(false);
+            followController.SetActive(false);
+        }
+
+        if (followAlong == true)
+        {
+            setWriteTrue.enabled = false;
+            particleController.SetActive(false);
+            primitivesController.SetActive(false);
+            player.SetActive(false);
+            mainCamera.enabled = true;
+        }
+    }
+
+
+}
